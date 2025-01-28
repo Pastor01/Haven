f51bd44adf99689a3f7d908ebeaddd4a826a4f09
Nicolas Hergesheimer <nicoherge@gmail.com>
2025-01-27 23:09:58 -0300

    Add analize functionality to controllers
    Add html section to analize the books. Also include implementation to fetch and then analize information from a given book

--- backend/src/main/java/com/haven/gutenbergBooks/controler/BookController.java
+++ backend/src/main/java/com/haven/gutenbergBooks/controler/BookController.java
@@ -2,6 +2,8 @@ package com.haven.gutenbergBooks.controler;
 
 import com.haven.gutenbergBooks.model.Book;
 import com.haven.gutenbergBooks.services.BookService;
+import com.haven.gutenbergBooks.services.SambaService;
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,15 +13,28 @@ import org.springframework.web.bind.annotation.*;
 public class BookController {
 
     private final BookService bookService;
+    private final SambaService sambaService;
 
-    public BookController(BookService bookService) {
+    public BookController(BookService bookService, SambaService sambaService) {
         this.bookService = bookService;
+        this.sambaService = sambaService;
     }
 
     @GetMapping("/{bookId}")
     public ResponseEntity<Book> fetchBook(@PathVariable String bookId) {
         Book book = bookService.fetchBook(bookId);
         return ResponseEntity.ok(book);
+    }
+
+    @PostMapping("/analyze")
+    public ResponseEntity<?> analyzeBook(@RequestBody Map<String, String> request) {
+        String bookId = request.get("bookId");
+
+        Book book = bookService.fetchBook(bookId);
+
+        String analysisResult = sambaService.sendRequest(book.getContent());
+
+        return ResponseEntity.ok(Map.of("analysisResult", analysisResult));
     }
 }
 
